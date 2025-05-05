8ab377d8bbc26386813bdc37e4eb86834651579f
stashart <stanislav.hart@gmail.com>
2025-05-05 18:03:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -166,9 +166,11 @@ def handle_voice(message):
             bot.reply_to(message, reply_text)
 
     except Exception as e:
-        print(f"Ошибка при обработке голосового через Deepgram: {e}")
-        if user_id == CREATOR_ID:
-            bot.reply_to(message, "⚠️ Не получилось обработать голосовое")
+    import traceback
+    error_text = traceback.format_exc()
+    print(f"Ошибка при обработке голосового через Deepgram:\n{error_text}")
+    if user_id == CREATOR_ID:
+        bot.reply_to(message, "⚠️ Не получилось обработать голосовое\n" + str(e))
             
 # Webhook
 @app.route(f"/{API_TOKEN}", methods=["POST"])
