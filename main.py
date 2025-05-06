c1cd45b2c2770cbdf547819894b795632497c2b3
stashart <stanislav.hart@gmail.com>
2025-05-06 11:23:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 import telebot
 from flask import Flask, request
 import subprocess
-from deepgram import DeepgramClient, FileSource, PrerecordedOptions
+from deepgram import Deepgram
 
 # Проверка наличия ffmpeg
 try:
@@ -131,7 +131,7 @@ def handle_voice(message):
                 "model": "nova",
                 "language": "ru"
             }
-            response = dg.listen.prerecorded.v("1").transcribe_file(source=source, options=options)
+            response = dg.listen.prerecorded.transcribe_file(source=source, options=options)
 
         user_input = response["results"]["channels"][0]["alternatives"][0].get("transcript", "").strip()
         if not user_input:
